ed372bbbf8a890a9b268c80cf8ef4e21594a5e95
MastroCoder <fe03mastromauro@gmail.com>
2024-04-05 20:02:36 -0300

    Início Login e validação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule, RouterOutlet, RouterLink, 
+    RouterLinkActive, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email],),
+    password: new FormControl('', [Validators.min(6), Validators.max(15)])
+  })
+
+  onSubmit(){
+    console.warn(this.loginForm.value.email);
+    console.warn(this.loginForm.value.password);
+  }
+}
